d8b3d7d1f24cdb95ee83a7e3c5a8440a89b91ce8
Xiang Wang <ramwin@qq.com>
2024-02-22 20:40:52 +0800

    feat: student and mutate function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-{
-  "compileOptions": {
-    "outputDir": "dist",
-  },
-  "include": [
-    "src/**/*",
-  ]
+export interface Student {
+  id: number
+  name: string
+  age: number
 }
+
+export const alice: Student = {
+  id: 1,
+  name: "alice",
+  age: 18,
+}
+
+export const bob: Student = {
+  id: 2,
+  name: "bob",
+  age: 19,
+}
+
+console.log(alice);
+
+
+export const mutate = <K extends string, O extends {[KK in K]: unknown}>(a: O, b:O, keys: K[]) => {
+  keys.forEach(key=> {
+    a[key] = b[key]
+  })
+}
+
+mutate(alice, bob, ["name"])
+
+console.log(alice);
